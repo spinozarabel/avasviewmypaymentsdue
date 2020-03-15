5f763fb07781b808ed4c9a7bd35227293fb9a77c
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2020-03-15 12:34:24 -0700

    Initial commit of settings.php and version.php

---
+++ settings.php
@@ -0,0 +1,13 @@
+<?php
+//
+// defines configuration settings for the moodle_block_avasviewmypaymentsdue
+// Madhu Avasarala
+//
+defined('MOODLE_INTERNAL') || die;
+
+if ($ADMIN->fulltree)
+{
+	$settings->add(new admin_setting_configtext('block_avasviewmypaymentsdue/profile_field_shortname', 'Short Name of User Profile Field fees',
+                'Enter the short name of the user profile field fees containing JSON encoded data (max 40 chars)', 'payments', PARAM_RAW, 40));
+
+}
